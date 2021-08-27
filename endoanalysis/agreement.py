Made dataset fixing notebook and started calculation of mean radius

endoanalysis/agreement.py
@@ -1,7 +1,7 @@
 import itertools
 import copy
 import numpy as np
-
+from tqdm import tqdm
 
 
 class SimilarityMeasure:
@@ -199,12 +199,14 @@ def merge_samples(samples_image):
     '''
     samples_merged = copy.deepcopy(samples_image)
     num_experts = samples_merged.shape[1]
+  
     for i, sample in enumerate(samples_merged[1:], start=1):
         for j, base_sample in enumerate(samples_merged[0:i]):
             compat, new_sample = glue_samples(sample, base_sample)
             if compat:
                 samples_merged[j] = new_sample
                 samples_merged[i] = np.array([-1] * num_experts)
+
     return samples_merged     
 
 
@@ -254,7 +256,7 @@ def compose_relaibility_matrix(objects_image, objects_classes, experts_list, sim
     endoanalysis.agreement.compose_samples_as_pairs
     endoanalysis.agreement.merge_samples
     '''
-    
+
     samples_image = []
 
     for expert_i, expert_j in itertools.combinations(range(len(experts_list)), 2):
@@ -282,6 +284,8 @@ def compose_relaibility_matrix(objects_image, objects_classes, experts_list, sim
 
     samples_image = np.vstack(samples_image).astype(int)
     
+
+    
     #sorting with the similarities, so the pairs with the largest similarity goes to the beginning
     samples_image = samples_image[np.argsort(samples_image[:,0])][::-1]
     
@@ -290,6 +294,8 @@ def compose_relaibility_matrix(objects_image, objects_classes, experts_list, sim
     
     #merging the samples
     samples_image = merge_samples(samples_image)
+    
+
       
     #erasing the samples with (-1, -1, ..., -1) signatures     
     samples_image = samples_image[np.where(np.prod(samples_image==-1, axis=1) == False)]     
@@ -346,20 +352,22 @@ def compose_batch_relaibility_matrix(
     
     
     reliability_matrix = []
+    print("Composing relaibility matrix")
+    with tqdm(total = len(images_indexes)) as pbar:
+        for image_i in images_indexes:
 
-    for image_i in images_indexes:
+            rel_matrix_image = compose_relaibility_matrix( 
+                {expert : objects_batches[expert].from_image(image_i) for expert in experts_list},
+                {expert : objects_batches[expert].from_image(image_i).classes() for expert in experts_list},
+                experts_list,
+                similarity,
+                similarity_thresh=sim_thresh
+            )
+            reliability_matrix.append(rel_matrix_image)
+            pbar.update()
 
-        rel_matrix_image = compose_relaibility_matrix( 
-            {expert : objects_batches[expert].from_image(image_i) for expert in experts_list},
-            {expert : objects_batches[expert].from_image(image_i).classes() for expert in experts_list},
-            experts_list,
-            similarity,
-            similarity_thresh=sim_thresh
-        )
-        reliability_matrix.append(rel_matrix_image)
-        
     reliability_matrix = np.vstack(reliability_matrix).transpose()
-    
+
     
     if class_agnostic:
         reliability_matrix[np.where(reliability_matrix!=-1)] = 1

endoanalysis/datasets.py
@@ -3,47 +3,40 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import patches, cm
-import albumentations as A
 
 from endoanalysis.keypoints import KeypointsTruthArray, KeypointsTruthBatchArray, keypoints_list_to_batch, load_keypoints
 
 
-def label_path_from_image_path(image_path):
-
-    image_path_parts = os.path.normpath(image_path).split(os.sep)
-    label_path_parts = ["labels"] + image_path_parts[1:]
-
-    label_path = os.path.join(*label_path_parts)
-    label_path = label_path.split(".")[:-1]
-    label_path = ".".join(label_path + ["txt"])
-    return label_path
-
+def extract_images_and_labels_paths(images_list_file, labels_list_file):
     
-def parse_files_list(path_to_file_with_list):
-    images_paths = []
-    labels_paths = []
-    dataset_dir = os.path.dirname(path_to_file_with_list)
+    images_list_dir = os.path.dirname(images_list_file)
+    labels_list_dir = os.path.dirname(labels_list_file)
 
-    with open(path_to_file_with_list, "r") as image_list_file:
-        for line in image_list_file:
+    with open(images_list_file, "r") as images_file:
+        images = images_file.readlines()
+        images = [os.path.normpath(os.path.join(images_list_dir,x.strip())) for x in images]
+    with open(labels_list_file, "r") as labels_file:
+        labels = labels_file.readlines()
+        labels = [os.path.normpath(os.path.join(labels_list_dir,x.strip())) for x in labels]
+    
+    check_images_and_labels_pathes(images, labels)
+    
+    return images, labels
 
-            image_path = line.strip()
-            if not (
-                image_path.startswith("./images") or image_path.startswith("images")
-            ):
-                raise Exception("Wrong file path spectification: %s" % image_path)
+def check_images_and_labels_pathes(images_paths, labels_paths):
+    
 
-            if image_path.startswith("./images"):
-                image_path = image_path[2:]
-
-            images_paths.append(image_path)
-            
-            
-    images_paths.sort()
-    labels_paths = [os.path.join(dataset_dir, label_path_from_image_path(x)) for x in images_paths]
-    images_paths = [os.path.join(dataset_dir, x) for x in images_paths]
-
-    return images_paths, labels_paths
+    if len(images_paths) != len(labels_paths):
+        raise Exception("Numbers of images and labels are not equal")
+        
+    for image_path, labels_path in zip(images_paths, labels_paths):
+        dirname_image = os.path.dirname(image_path)
+        dirname_labels = os.path.dirname(labels_path)
+        filename_image = os.path.basename(image_path)
+        filename_labels = os.path.basename(labels_path)
+     
+        if ".".join(filename_image.split(".")[:-1]) != ".".join(filename_labels.split(".")[:-1]):
+            raise Exception("Different dirnames found: \n %s\n  %s"%(images_paths, labels_paths))
 
 
 def load_image(image_path):
@@ -54,15 +47,31 @@ def load_image(image_path):
 class PointsDataset:
     def __init__(
         self,
-        path_to_file_with_list,
+        images_list,
+        labels_list,
         keypoints_dtype=np.int16,
+        cmap_kwargs = {"cmap": cm.Set1, "period": 8}
     ):
 
-        self.path_to_file_with_list = path_to_file_with_list
+
+            
         self.keypoints_dtype = keypoints_dtype
         # reading images_list
-        self.images_paths, self.labels_paths = parse_files_list(path_to_file_with_list)
-
+        
+        if type(images_list) != type(labels_list):
+            raise Exception("images_list_files and labels_list_file should have the same type")
+            
+        if type(images_list) != list:
+            images_list = [images_list]
+            labels_list = [labels_list]
+        self.images_paths = []
+        self.labels_paths = []
+        for images_list_path, labels_list_path in zip(images_list, labels_list):
+            images_paths_current, labels_paths_current = extract_images_and_labels_paths(images_list_path, labels_list_path)
+            self.images_paths += images_paths_current
+            self.labels_paths +=labels_paths_current
+        self.cmap_kwargs = cmap_kwargs
+        
     def __len__(self):
         return len(self.images_paths)
 
@@ -101,9 +110,12 @@ class PointsDataset:
         image_key="image",
         print_labels=False,
         labels_kwargs={"radius": 3, "alpha": 1.0},
-        colormap_kwargs = {"cmap": cm.Set1, "period": 8} 
+        cmap_kwargs = None 
     ):
-
+        
+        if cmap_kwargs is None:
+            cmap_kwargs = self.cmap_kwargs
+            
         sample = self[x]
         image = sample[image_key]
         keypoints = sample["keypoints"]
@@ -117,10 +129,10 @@ class PointsDataset:
         if show_labels:
             for center_x, center_y, obj_class in zip(x_coords, y_coords, classes):
                 
-                if colormap_kwargs["period"] is None:
-                    color = colormap_kwargs["cmap"](obj_class)
+                if "period" not in cmap_kwargs:
+                    color = cmap_kwargs["cmap"][obj_class]
                 else:
-                    color = colormap_kwargs["cmap"](obj_class % colormap_kwargs["period"])
+                    color = cmap_kwargs["cmap"](obj_class % cmap_kwargs["period"])
                 
                 patch = patches.Circle((center_x, center_y), color=color, **labels_kwargs)
 

endoanalysis/keypoints.py
@@ -50,16 +50,8 @@ class KeypointsArray(np.ndarray):
         self._check_shape()
         self._check_dtype()
 
-#     def _check_shape(self):
-#         if len(self.shape) == 1 and self.shape[0] != 0:
-#             raise Exception("If KeypointsArray is empty, it should be one dimensional")
-#         elif len(self.shape) == 2 and self.shape[1] != self.expected_param_number:
-#             raise Exception(
-#                 "Non-empty %s should have the shape (num_images, %i)"
-#                 % (type(self), self.expected_param_number)
-#             )
     def _check_shape(self):
-#         print(self.shape)
+
         if len(self.shape) != 2:
             raise Exception(
                 " %s should have the shape (num_images, %i)"