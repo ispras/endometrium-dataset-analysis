e5746afa0d0063412f00d7b5fb82ea6fbaf92fdc
Ushakov Egor <ushakov@ispras.ru>
2021-12-06 16:50:55 +0300

    add coco

---
+++ endoanalysis/coco_transfer.py
@@ -0,0 +1,76 @@
+import os
+import json
+import shutil
+from posixpath import normpath
+import cv2
+from endoanalysis.datasets import parse_master_yaml
+
+
+
+def parse(path_to_yaml):
+    images = []
+    labels = []
+    lists = parse_master_yaml(path_to_yaml)
+    return lists['images_lists'], lists['labels_lists']
+
+
+def make_coco(path_to_yaml, output_dir=None, anno_name='annotation', return_ano=False):
+    coco_ano = {}
+    dir_path = os.path.dirname(path_to_yaml)
+    images_list, labels_list = parse(path_to_yaml)
+    images = []
+    annotation = []
+    image_check_list = set()
+    labels_check_list = set()
+    
+    for images_path in images_list:
+        norm_path = os.path.normpath(os.path.join(dir_path, images_path))
+        with open(norm_path, 'r') as images_list_again:
+            for image_path in images_list_again:
+                image = {}
+                image_name = image_path.rstrip()
+                dir_path_again = os.path.split(norm_path)[0]
+                norm_path_again = os.path.normpath(os.path.join(dir_path_again, image_name))
+                img = cv2.imread(norm_path_again)
+                image['height'] = img.shape[0]
+                image['width'] = img.shape[1]
+                image['file_name'] = os.path.split(image_name)[1]
+                image['id'] = int(os.path.splitext(image['file_name'])[0])
+                if image['id'] not in image_check_list:
+                    images.append(image)
+                    if output_dir:
+                        shutil.copy(norm_path_again, os.path.join(output_dir, image['file_name']))
+                    image_check_list.add(image['id'])
+
+    for labels_path in labels_list:
+        norm_path = os.path.normpath(os.path.join(dir_path, labels_path))
+        with open(norm_path, 'r') as labels_list_again:
+            
+            for label_path in labels_list_again:
+                label_name = label_path.rstrip()
+                dir_path_again = os.path.split(norm_path)[0]
+                norm_path_again = os.path.normpath(os.path.join(dir_path_again, label_name))
+                image_id = int(os.path.splitext(os.path.split(label_name)[1])[0])
+                if image_id in image_check_list and image_id not in labels_check_list:
+                    labels_check_list.add(image_id)
+                    with open(norm_path_again, 'r') as labels_for_image:
+                        for label in labels_for_image:
+                            labels = {}
+                            labels['num_keypoints'] = 1
+                            keypoints = label.strip().split(" ")
+                            keypoints = list(int(float(x)) for x in keypoints)
+                            labels['keypoints'] = keypoints[:-1]
+                            labels['image_id'] = image_id
+                            labels['bbox'] = labels['keypoints'] + labels['keypoints']
+                            labels['category_id'] = keypoints[-1]
+                            annotation.append(labels)
+        
+    coco_ano['images'] = images
+    coco_ano['annotations'] = annotation
+    if output_dir:
+        with open(f'{output_dir}/{anno_name}.json', 'w') as anno_file:
+            json.dump(coco_ano, anno_file)
+    if return_ano:
+        return coco_ano
+
+# make_coco('/home/ushakov/isp/mmdetection/train/train.yaml')
